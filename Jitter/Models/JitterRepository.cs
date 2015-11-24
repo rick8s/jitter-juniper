266b3edf58b7ac12a5160655ed379a81bc40e158
Rick8s <rick8s@hotmail.com>
2015-11-24 12:39:30 -0600

    added functionality for getting user out of JitterUsers

--- Jitter/Models/JitterRepository.cs
+++ Jitter/Models/JitterRepository.cs
@@ -25,5 +25,40 @@ namespace Jitter.Models
             var query = from users in _context.JitterUsers select users;
             return query.ToList();
         }
+
+        public JitterUser GetUserByHandle(string handle)
+        {
+            //SQL select * from Jitterusers where JitterUser.Handle = handle;
+            var query = from user in _context.JitterUsers where user.Handle == handle select user;
+            //IQueryable<JitterUser> query = from user in _context.JitterUsers where user.Handle == handle select user;
+            // we need to make sure there is exactly one user returned.
+            return query.SingleOrDefault<JitterUser>();
+
+        }
+
+        public bool IsHandleAvailable(string handle)
+        {
+            bool available = false;
+            try
+            {
+                JitterUser some_user = GetUserByHandle(handle);
+                if (some_user == null)
+                {
+                    available = true;
+                }
+            }
+            catch (InvalidOperationException){ }
+                      
+            return available;
+        }
+
+        public List<JitterUser> SearchByHandle(string handle)
+        {
+            //SQL select * from JitterUsers As users where users.Handle like %handle%;
+            var query = from user in _context.JitterUsers select user;
+            List<JitterUser> found_users = query.Where(user => user.Handle.Contains(handle)).ToList();
+            found_users.Sort();
+            return found_users;
+        }
     }
 }
