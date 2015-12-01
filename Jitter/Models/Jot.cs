10553789657ddf2ccf85231f0aa37f9ffdf9632f
Rick8s <rick8s@hotmail.com>
2015-12-01 09:01:57 -0600

    added search by name test functionality

--- Jitter/Models/Jot.cs
+++ Jitter/Models/Jot.cs
@@ -6,13 +6,23 @@ using System.Web;
 
 namespace Jitter.Models
 {
-    public class Jot
+    public class Jot : IComparable
     {
-        public object Author { get; set; }
+        public JitterUser Author { get; set; }
+        [Required]
         public string Content { get; set; }
         public DateTime Date { get; set; }
         [Key]
         public int JotId { get; set; }
         public string Picture { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            // down-casting this instance of an object
+            Jot other_jot = obj as Jot;
+            //multiply b/c DateTime's CompareTo Sorts Ascending by default.
+            //we want descending sort.
+            return -1*(this.Date.CompareTo(other_jot.Date));
+        }
     }
 }
