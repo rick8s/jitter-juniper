fd9694d166a0a0f5c176692a517c1ef86c223c89
Rick8s <rick8s@hotmail.com>
2015-12-03 12:46:04 -0600

    added/displayed list of names to the About page. Added notation to HomeController.

--- Jitter/Controllers/HomeController.cs
+++ Jitter/Controllers/HomeController.cs
@@ -16,6 +16,13 @@ namespace Jitter.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            // 1. create or get a list of things
+            List<string> my_list_of_things = new List<string>();
+            my_list_of_things.Add("Timmy");
+            my_list_of_things.Add("Chef");
+            my_list_of_things.Add("Greg");
+
+            return View(my_list_of_things);
 
             return View();
         }
